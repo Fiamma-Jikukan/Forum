d1a40ca9d8ed5e9226e57b82c7a947e19103b550
Fiamma <stavtxxx1@gmail.com>
2023-05-14 15:41:44 +0300

    works-I-think

--- server.js
+++ server.js
@@ -2,34 +2,84 @@ const http = require("http");
 const express = require("express")
 const bodyParser = require("body-parser")
 require('dotenv').config()
+const cookieParser = require("cookie-parser")
 
 const username = "Fiamma";
 const password = "IKilledThePope123";
+const siteControl = { authenticated: false, message: "Please log in" }
+
 
 const app = express();
 // middleware
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use("/public", express.static(__dirname + "/public"));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+    console.log(app.locals.session_id);
+    const { cookies } = req
+    try {
+        if (cookies.session_id == app.locals.session_id) {
+            siteControl.authenticated = true;
+            console.log(1);
+            console.log(cookies.session_id);
+            console.log(app.locals.session_id);
+        } else {
+            siteControl.authenticated = false;
+            console.log(2);
+            console.log(cookies.session_id);
+            console.log(app.locals.session_id);
+        }
+    } catch {
+        console.log("error");
+        next();
+    }
+    console.log("the use thing");
+    console.log(siteControl.authenticated);
+    next()
+})
+
 // get requests
 app.get("/", (req, res) => {
-    res.render('index', { title: 'Welcome To My Site', message: 'Please log in' });
+    res.render('index', siteControl);
 })
 // post requests
 app.post("/login", async (req, res) => {
     try {
+        app.locals.session_id = 0
         if (req.body.username === username && req.body.password === password) {
-            res.render('index', { title: 'You are logged in!', message: ':)' });
+            const session_id = Math.floor(Math.random() * 1000000);
+            res.cookie('session_id', session_id);
+            app.locals.session_id = session_id
+            console.log("this is the session id: " + app.locals.session_id);
+            console.log("this is the session id: " + req.cookies.session_id);
+            res.redirect('/');
+            res.end()
         } else {
-            res.render('index', { title: 'wrong username or password!', message: ':(' });
+            console.log(req.cookies);
+            siteControl.message = "wrong username or password"
+            res.redirect('/');
         }
     }
     catch (err) {
         res.render('index', { title: 'Error', message: 'Please log in' });
     }
 })
+
+app.post("/logout", async (req, res) => {
+    try {
+        await res.clearCookie('session_id');
+        app.locals.session_id = 0
+        console.log("Cookie cleared")
+        res.redirect('/');
+    }
+    catch (err) {
+        res.send('error');
+    }
+})
+
 // port
 app.listen(3000, () => {
     console.log("running on port 3000");
