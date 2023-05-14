44e7eaf58a7fd184384e4e0c2fa28c20236609ef
Fiamma <stavtxxx1@gmail.com>
2023-05-14 10:02:58 +0300

    start-the-login

--- server.js
+++ server.js
@@ -3,24 +3,38 @@ const express = require("express")
 const bodyParser = require("body-parser")
 require('dotenv').config()
 
+const username = "Fiamma";
+const password = "IKilledThePope123";
+
 const app = express();
-//middleware
+// middleware
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-    console.log(req.method + " " + req.path + "-" + req.ip);
-    next()
-})
+// app.use((req, res, next) => {
+//     console.log(req.method + " " + req.path + "-" + req.ip);
+//     next()
+// })
 app.use("/public", express.static(__dirname + "/public"));
-//get requests
+// get requests
 app.get("/", (req, res) => {
-    res.render('index', { title: 'hi', message: 'Please log in' });
+    res.render('index', { title: 'Welcome To My Site', message: 'Please log in' });
     //    res.sendFile(absolute);
 })
-//post requests
-
-//port
+// post requests
+app.post("/login", async (req, res) => {
+    try {
+        if (req.body.username === username && req.body.password === password) {
+            res.render('index', { title: 'You are logged in!', message: ':)' });
+        } else {
+            res.render('index', { title: 'wrong username or password!', message: ':(' });
+        }
+    }
+    catch (err) {
+        res.render('index', { title: 'Error', message: 'Please log in' });
+    }
+})
+// port
 app.listen(3000, () => {
     console.log("running on port 3000");
 });
