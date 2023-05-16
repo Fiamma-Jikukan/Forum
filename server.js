73d162cd8afceffbb9418c94db6365e3d9770d69
Fiamma <stavtxxx1@gmail.com>
2023-05-16 19:13:12 +0300

    add-database

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const http = require("http");
 const express = require("express")
+const session = require("express-session")
 const bodyParser = require("body-parser")
 require('dotenv').config()
 const cookieParser = require("cookie-parser")
 
-const { users, sessions } = require('./users')
+const { Schema,
+    userSchema,
+    User,
+    users,
+    sessions,
+    connect,
+    sessionSchema,
+    Session } = require("./users.js")
+
 
 const app = express();
 // middleware
@@ -15,40 +24,88 @@ app.use("/public", express.static(__dirname + "/public"));
 app.use(cookieParser());
 
 // get requests
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     const { cookies } = req
-    res.render('index', sessions[cookies.session_id]);
+    try {
+        const currentSession = await Session.findById(cookies.session_id)
+        if (currentSession === null) {
+            res.render('index');
+        } else {
+            res.render('index', currentSession);
+        }
+
+    } catch (err) {
+        res.status(500).json({ result: 'bad', error: err.message })
+    }
+
 })
 // post requests
 app.post("/signup", async (req, res) => {
+    const signupUser = await User.findOne({ username: req.body.username })
+    try {
+        if (signupUser) {
+            console.log("user not added");
+            res.redirect('/');
+        } else {
+            const newUser = await User.create({
+                username: req.body.username,
+                password: req.body.password,
+            });
+            console.log("user added");
+            res.redirect('/');
+        }
 
-    users[req.body.username] = req.body.password;
-    res.redirect('/');
+    } catch (err) {
+        res.status(500).json({ result: 'bad', error: err.message })
+    }
+
+    // users[req.body.username] = req.body.password;
+    // res.redirect('/');
 })
 
 app.post("/login", async (req, res) => {
-
-    if (users[(req.body.username)]) {
-        if (users[req.body.username] === req.body.password) {
-            const session_id = Math.floor(Math.random() * 1000000);
-            sessions[session_id] = { user: req.body.username }
-            res.cookie('session_id', session_id);
-            res.redirect('/');
+    const loginuser = await User.findOne({ username: req.body.username })
+    try {
+        if (loginuser) {
+            if (loginuser.password === req.body.password) {
+                const newSession = await Session.create({
+                    user: req.body.username,
+                });
+                console.log(newSession.id);
+                console.log("session added");
+                res.cookie('session_id', newSession.id);
+            } else {
+                res.redirect('/');
+            }
         } else {
-            res.redirect('/');
+            res.render('index', sessions);
         }
-    } else {
-        res.render('index', sessions);
+    } catch (err) {
+        res.status(500).json({ result: 'bad', error: err.message })
     }
+
+    res.redirect('/');
+
 
 
 })
 
 app.post("/logout", async (req, res) => {
     const { cookies } = req
-    delete sessions[cookies.session_id]
-    res.clearCookie('session_id');
-    res.redirect('/');
+    try {
+        const currentSession = await Session.findById(cookies.session_id)
+        const deleteSession = await Session.findOneAndDelete(currentSession.id)
+        res.clearCookie('session_id');
+        console.log("session removed");
+        res.redirect('/');
+
+    } catch (err) {
+        const currentSession = await Session.findById(cookies.session_id)
+        console.log(currentSession);
+        res.status(500).json({ result: 'bad', error: err.message })
+
+    }
+
 })
 
 // port
--- users.js
+++ users.js
@@ -1,3 +1,31 @@
+const mongoose = require("mongoose")
+
+require('dotenv').config()
+
+const connect = mongoose.connect(process.env.DATABASE)
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+let User = mongoose.model("User", userSchema);
+
+const sessionSchema = new Schema({
+    user: {
+        type: Object,
+        required: true
+    },
+})
+let Session = mongoose.model("Session", sessionSchema);
+
+
 const users = {
     "Fiamma": "IKilledThePope123",
     "Madara": "KageJoke",
@@ -9,6 +37,12 @@ const sessions = {
 }
 
 module.exports = {
+    Schema,
+    userSchema,
+    User,
     users,
-    sessions
+    sessions,
+    connect,
+    sessionSchema,
+    Session
 }
